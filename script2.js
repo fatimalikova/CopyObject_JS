e16c9ab4a5bf099a4cc46bc96211427c72b8b722
fatimalikova <fatimakhm@code.edu.az>
2026-03-06 23:57:04 +0400

    practised copy types

---
+++ script2.js
@@ -0,0 +1,13 @@
+let user ={
+    name:"Fatima",
+    surname:"Malikova",
+    age: 19
+};
+
+// const item = JSON.stringify(user);
+// const result =JSON.parse(item);
+// result.name="Alina"
+// console.log(result)
+
+
+console.log(Object.assign({address:"Baku"},user));
